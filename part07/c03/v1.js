234c6d3367030fd4de72b1b80f149cf6db486908
hyoyeong <kimhyo089@naver.com>
2018-04-29 18:02:06 +0900

    이미지슬라이더

---
+++ part07/c03/v1.js
@@ -0,0 +1,34 @@
+
+//2-1. 이미지 요소 상태 초기화
+
+// step #02-01
+function ImageSlider(selector){
+
+	this.$imageSlider = null;
+	this.$images = null;
+
+	this.init(selector);
+	this.initImages();
+}
+
+
+/* step #02-01
+* 요소 초기화
+*/
+ImageSlider.prototype.init=function(selector){
+	this.$imageSlider = $(selector);
+	this.$images = this.$imageSlider.find(".slider-list img");
+}
+
+/* step #02-01
+* 이미지 요소 초기화
+*/
+ImageSlider.prototype.initImages=function(selector){
+	this.$images.each(function(){
+		$(this).css({
+			opacity:0.0
+		})
+	})
+};
+
+
